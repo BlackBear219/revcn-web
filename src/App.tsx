5167a78bd1dfcdd419dba1c8e26677e53ade4b50
XiangrongXu <xiangrong_xxr@outlook.com>
2023-07-28 21:32:34 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useStyles } from './AppStyle'
 import { NavigationBar } from './components/NavigationBar/NavigationBar'
