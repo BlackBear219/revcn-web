ff98c0d6f049aed70078088cf8e1ce4bdf4f9cbe
Xiangrong Xu <xiangrong_xxr@outlook.com>
2023-08-03 02:50:01 +0000

    try to fix cross online

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    cors: true,
+    host: true,
     proxy: {
       "/api": {
         target: "https://revcn.azurewebsites.net",
