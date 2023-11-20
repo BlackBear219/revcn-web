fb26fa6ce02b947a2afa0a3bebfa4c1e5fc75332
XiangrongXu <xiangrong_xxr@outlook.com>
2023-11-20 12:25:27 +0800

    Enable revcn GPT

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,11 @@
-import { SelectTabData, SelectTabEvent, Tab, TabList } from "@fluentui/react-components";
-import { BotSparkle24Filled, CatchUp24Filled, Home24Filled } from "@fluentui/react-icons";
+import { Button, SelectTabData, SelectTabEvent, Tab, TabList } from "@fluentui/react-components";
+import { BotSparkle24Regular, CatchUp24Filled, Home24Filled } from "@fluentui/react-icons";
 import { FunctionComponent, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+
+const aiClick = () => {
+    window.location.href = "https://revcngpt404a5bc1-dbc6-9f63-05a8-17ff37e78e9d.azurewebsites.net";
+}
 
 export const NavigationBar: FunctionComponent = () => {
     let location = useLocation();
@@ -15,20 +19,22 @@ export const NavigationBar: FunctionComponent = () => {
     };
 
     return (
-        <TabList
-            selectedValue={selectedValue}
-            onTabSelect={onTabSelect}
-            size="large"
-        >
-            <Tab icon={<Home24Filled />} value="/home">
-                首页
-            </Tab>
-            <Tab icon={<CatchUp24Filled />} value="/houseMetrics">
-                湖西银座房屋余量
-            </Tab>
-            <Tab icon={<BotSparkle24Filled />} value="/aiAssitant" disabled>
-                AI助手（敬请期待）
-            </Tab>
-        </TabList>
+        <>
+            <TabList
+                selectedValue={selectedValue}
+                onTabSelect={onTabSelect}
+                size="large"
+            >
+                <Tab icon={<Home24Filled />} value="/home">
+                    首页
+                </Tab>
+                <Tab icon={<CatchUp24Filled />} value="/houseMetrics">
+                    湖西银座房屋余量
+                </Tab>
+            </TabList>
+            <Button appearance="transparent" icon={<BotSparkle24Regular />} onClick={aiClick}>
+                AI助手
+            </Button>
+        </>
     );
 }
