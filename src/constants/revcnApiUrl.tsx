20063a1ab008cf67c206e6946d805ba8223acc00
XiangrongXu🐻 <xiangrong.xxr@outlook.com>
2024-04-12 14:04:15 -0700

    refine api route

--- src/constants/revcnApiUrl.tsx
+++ src/constants/revcnApiUrl.tsx
@@ -1,5 +1,9 @@
 export const baseUrl = import.meta.env.PROD ? "https://revcn.azurewebsites.net" : ""
 
-export const totalHouseMetricApi = "/api/metrics/totalHouse";
+export const totalHouseMetricsApi = "/api/metric/metrics/totalHouse";
 
-export const specialHouseMetricApi = "/api/metrics/specialHouse";
+export const specialHouseMetricsApi = "/api/metric/metrics/specialHouse";
+
+export const totalHouseLastMetricApi = "/api/metric/lastMetric/totalHouse";
+
+export const specialHouseLastMetricApi = "/api/metric/lastMetric/specialHouse";
--- src/services/houseMetricsServices.tsx
+++ src/services/houseMetricsServices.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { baseUrl, specialHouseMetricApi, totalHouseMetricApi } from "../constants/revcnApiUrl";
+import { baseUrl, specialHouseMetricsApi, totalHouseMetricsApi } from "../constants/revcnApiUrl";
 import { metric, metricType } from "../models/metric";
 import { revcnResponse } from "../models/revcnResponse";
 import { newIllegalResponse } from "../util/newIllegalResponse";
@@ -7,7 +7,7 @@ import { newIllegalResponse } from "../util/newIllegalResponse";
 export const getHouseMetrics = async (metrcType: metricType) : Promise<revcnResponse<metric[]>> => {
     try {
         var targetUrl = metrcType === metricType.HOUSE_AMOUNT_SPECIAL ?
-            specialHouseMetricApi : totalHouseMetricApi;
+            specialHouseMetricsApi : totalHouseMetricsApi;
         var targetUrl = baseUrl + targetUrl;
         var response = await axios.get(targetUrl);
         var result = response.data as revcnResponse<metric[]>;
